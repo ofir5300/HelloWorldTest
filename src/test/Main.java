37a8d682b731bcbcdb98a76cfb8fcb34a52a94ee
Dekel <dekelglass@gmail.com>
2019-07-08 17:18:36 +0300

    wrapitup

--- src/test/Main.java
+++ src/test/Main.java
@@ -8,11 +8,15 @@ public class Main {
         System.out.println("Ah sheli hayakar");
         System.out.println("test2");
 
+
+        ???
         // dekel branch created
-        System.out.println("master update");
-        // dekel computer
-        System.out.println("Ofir");
+        System.out.println("dekel bracnch updated");
+        // amit computer
+    }
 
 
+    {
+        inal dinak
     }
 }
